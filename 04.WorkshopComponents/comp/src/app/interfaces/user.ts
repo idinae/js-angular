790615fbcf4977b00a339ee861f3c4fea41d62b8
idinae <iddinae@gmail.com>
2020-11-21 22:16:34 +0200

    interfaces added

---
+++ 04.WorkshopComponents/comp/src/app/interfaces/user.ts
@@ -0,0 +1,12 @@
+export interface IUser {
+    themes: string[]; //масив от стрингове
+    posts: string[];
+    _id: string;
+    tel: string;
+    email: string;
+    username: string;
+    password: string;
+    created_at: string;
+    updatedAt: string;
+    __v: number;
+}
