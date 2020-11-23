dc53696c3eb78ffeec2f7fbbc165f0526423b8c3
idinae <iddinae@gmail.com>
2020-11-23 23:50:27 +0200

    new app routing

---
+++ 05.Modules&Routing/app-with-router/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [];
+
+
+export const AppRoutingModule = RouterModule.forRoot(routes);
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
