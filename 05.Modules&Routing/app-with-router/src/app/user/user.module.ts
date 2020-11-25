d0366b9a1ee4e908106bff9c9fc0cdc7548429d8
idinae <iddinae@gmail.com>
2020-11-26 00:14:50 +0200

    user list added

--- 05.Modules&Routing/app-with-router/src/app/user/user.module.ts
+++ 05.Modules&Routing/app-with-router/src/app/user/user.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
 import { UserRoutingModule } from './user-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ListComponent, DetailComponent],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    SharedModule
   ]
 })
 export class UserModule { }
