fab7971c35b24c4370310dbe4102d0fdff3de77c
idinae <iddinae@gmail.com>
2020-11-25 23:54:53 +0200

    HttpClientModule added

--- 05.Modules&Routing/app-with-router/src/app/app.module.ts
+++ 05.Modules&Routing/app-with-router/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,8 @@ import { UserModule } from './user/user.module';
     AppRoutingModule,
     CoreModule,
     SharedModule,
-    UserModule
+    UserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
