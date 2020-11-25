37d1a3732f44c4cb1feabc2046aa2a67be211942
idinae <iddinae@gmail.com>
2020-11-26 00:02:51 +0200

    loadUsers added

--- 05.Modules&Routing/app-with-router/src/app/user/list/list.component.ts
+++ 05.Modules&Routing/app-with-router/src/app/user/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  userList = null;
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.loadUsers().subscribe(userList => {
+      this.userList = userList;
+    });
   }
-
 }
