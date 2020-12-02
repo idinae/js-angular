2514c7885662209fd8205c1013252c706055d4a9
idinae <iddinae@gmail.com>
2020-12-03 00:31:42 +0200

    ngStyle directive added

--- .Udemy/my-app/src/app/server/server.component.ts
+++ .Udemy/my-app/src/app/server/server.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-server', //string, unique; //'[app-server]' --> selected <div app-server></div>
     templateUrl: './server.component.html',
-    styleUrls: ['./server.component.css']
+    //styleUrls: ['./server.component.css']
+    styles: [`
+        .online {
+            color: white;
+        }
+    `]
 })
 export class ServerComponent {
     serverId: number = 10;
@@ -25,7 +30,7 @@ export class ServerComponent {
             this.allowNewServer = true;
         }, 2000);
         this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
-        
+
     }
 
     onCreateServer() {
