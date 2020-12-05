421e053d8d6bfeb0d9f950f8f55ae2c8451009a7
idinae <iddinae@gmail.com>
2020-12-05 21:54:57 +0200

    recipe-list ok

--- .Udemy/new-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ .Udemy/new-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://d2v9mhsiek5lbq.cloudfront.net/eyJidWNrZXQiOiJsb21hLW1lZGlhLXVrIiwia2V5IjoiZm9vZG5ldHdvcmstaW1hZ2UtNmI0Y2FlYmItNjk4Mi00MGI1LThhMjYtZjM4ZDRmNWMyNjhlLmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjc1MCwiaGVpZ2h0Ijo0MjIsImZpdCI6ImNvdmVyIn19fQ=='),
+    new Recipe('A Test Recipe 2', 'This is a new test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563')
+
+  ];
 
   constructor() { }
 
