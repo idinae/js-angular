94247ab9b33fb38c7d9d994ee425a63c38f31072
idinae <iddinae@gmail.com>
2020-12-10 21:56:49 +0200

    lyfe cycle hooks

--- .Udemy/data-bind-app/src/app/server-element/server-element.component.ts
+++ .Udemy/data-bind-app/src/app/server-element/server-element.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, ViewEncapsulation, SimpleChange, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated //None, Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges { //a good practice to inplement OnInit, OnChanges
   @Input('srvElement') element: {type: string, name: string, content: string}; //в скобите е allias на елемента, с който се достъпва отвън
-  constructor() { }
+ 
+ 
+  constructor() { 
+    console.log('constructor called!');
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called!');
   }
 
 }
