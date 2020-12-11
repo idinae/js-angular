9c1305102c176261fcc3b78a76cdf11ef8a23173
idinae <iddinae@gmail.com>
2020-12-11 22:19:03 +0200

    some changes

--- .Udemy/new-app/src/app/app.component.ts
+++ .Udemy/new-app/src/app/app.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'new-app';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
