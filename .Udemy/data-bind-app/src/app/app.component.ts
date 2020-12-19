dd76e3bcb3b03dcadb8345a8d5a5bff15a3fa32c
idinae <iddinae@gmail.com>
2020-12-19 23:41:44 +0200

    services test

--- .Udemy/data-bind-app/src/app/app.component.ts
+++ .Udemy/data-bind-app/src/app/app.component.ts
@@ -27,4 +27,26 @@ export class AppComponent {
   onIntervalFired(firedNumber: number) {
     console.log(firedNumber)
   }
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+  onAccountAdded(newAccount: {name: string, status: string}) {
+    this.accounts.push(newAccount);
+  }
+
+  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
+    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  }
 }
--- .Udemy/data-bind-app/src/app/app.module.ts
+++ .Udemy/data-bind-app/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
 import { EvenComponent } from './even/even.component';
 import { GameControlComponent } from './game-control/game-control.component';
 import { OddComponent } from './odd/odd.component';
+import { NewAccountComponent } from './new-account/new-account.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,9 @@ import { OddComponent } from './odd/odd.component';
     ServerElementComponent,
     EvenComponent,
     GameControlComponent,
-    OddComponent
+    OddComponent,
+    NewAccountComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
