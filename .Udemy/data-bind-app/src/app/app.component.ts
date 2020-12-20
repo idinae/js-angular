e60c2f446dd078a91ddcd237f1b010c352d6ec73
idinae <iddinae@gmail.com>
2020-12-20 23:52:09 +0200

    ok with error

--- .Udemy/data-bind-app/src/app/app.component.ts
+++ .Udemy/data-bind-app/src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccountsService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test:'}];
 
   onServerAdded(serverData: {serverName: string, serverContent: string}) {
@@ -27,26 +29,13 @@ export class AppComponent {
   onIntervalFired(firedNumber: number) {
     console.log(firedNumber)
   }
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
 
-  onAccountAdded(newAccount: {name: string, status: string}) {
-    this.accounts.push(newAccount);
-  }
 
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  accounts: {name: string, status: string}[] = [];
+
+  constructor(private accountsService: AccountsService) {}
+
+  ngOnInit() {
+    this.accounts = this.accountsService.accounts;
   }
 }
--- .Udemy/data-bind-app/src/app/new-account/new-account.component.ts
+++ .Udemy/data-bind-app/src/app/new-account/new-account.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 // import { LoggingService } from '../logging.service';
 
 @Component({
@@ -9,17 +10,19 @@ import { LoggingService } from '../logging.service';
   providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
+  // @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
 
-  constructor(private loggingService: LoggingService) {}
+  constructor(private loggingService: LoggingService,
+    private accountsService: AccountsService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    // this.accountAdded.emit({
+    //   name: accountName,
+    //   status: accountStatus
+    // });
     // const service = new LoggingService(); //wrongly to instantiate a new service this way
     // service.logStatusChange(accountStatus);
+    this.accountsService.addAccount(accountName, accountStatus);
     this.loggingService.logStatusChange(accountStatus);
   }
 }
