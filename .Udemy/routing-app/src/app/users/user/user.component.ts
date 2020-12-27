b52201e3eb274727ecda3b7a49aebb4519fa1dc1
idinae <iddinae@gmail.com>
2020-12-28 00:35:31 +0200

    params change subscribe

--- .Udemy/routing-app/src/app/users/user/user.component.ts
+++ .Udemy/routing-app/src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -15,7 +15,16 @@ export class UserComponent implements OnInit {
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
-    }
+    };
+    //this function will update our user object whenever the parameter change
+    //use this approach only if you expect the params to be changed from whithin a component
+    this.route.params //the event will fire when the params change
+      .subscribe(
+        (params: Params) => { //takes the new params
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        } 
+      );
   }
 
 }
